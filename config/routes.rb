1870b2dc6d2f52a746fc6a7c7f7e9d9ada5c7c79
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2023-02-22 11:48:16 +0100

    config pundit & cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'nouvelles#index'
+  resources :nouvelles, except: [:edit, :update]
+  get "about", to: "pages#about"
+  get "info", to: "pages#info"
+  get "media", to: "pages#media"
+  get "enseignement", to: "pages#enseignement"
+  get "bibliographie", to: "pages#bibliographie"
+  get "articles", to: "pages#articles"
+  get "dojos", to: "pages#dojos"
 end
