1e71ce9b92c95cf26cb926499475c4bfc4e7faf5
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2024-03-26 12:21:13 +0100

    duplicate migration fixed

--- db/migrate/20230222101140_create_nouvelles.rb
+++
@@ -1,8 +0,0 @@
-class CreateNouvelles < ActiveRecord::Migration[7.0]
-  def change
-    create_table :nouvelles do |t|
-      t.references :user, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
