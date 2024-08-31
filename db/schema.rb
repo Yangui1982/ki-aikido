fbdcf37d04afd906c543b8e39bb694270b1796c2
Jean Guillaume Vicente <jg.vicente@wanadoo.fr>
2024-08-31 10:16:42 +0200

    inital commit new computer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_25_145151) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_31_081441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,11 +52,27 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_25_145151) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
   create_table "nouvelles", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_nouvelles_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_photos_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,5 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_25_145151) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "events", "users"
   add_foreign_key "nouvelles", "users"
+  add_foreign_key "photos", "events"
 end
