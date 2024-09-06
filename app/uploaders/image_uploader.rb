007c637c51f3a11667312df16cc862f3cdc068bf
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2024-09-06 22:43:31 +0200

    config initializer

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,9 +7,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumbnail do
     process resize_to_fit: [150, 150]
   end
-
-  # def public_id
-  #   # "events/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  #   "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/#{Time.now.to_i}"
-  # end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,35 +1,18 @@
-require 'cloudinary'
-require 'cloudinary/uploader'
-require 'cloudinary/utils'
 require 'carrierwave/orm/activerecord'
+require 'cloudinary'
 
 CarrierWave.configure do |config|
-  config.storage = :cloudinary
-  config.cloudinary_credentials = {
-    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-    api_key:    ENV['CLOUDINARY_API_KEY'],
-    api_secret: ENV['CLOUDINARY_API_SECRET'],
-    secure:     true
-  }
+  config.cache_storage = :file
 
-  # Utilisez un dossier temporaire spécifique à Heroku
-  config.cache_dir = "#{Rails.root}/tmp/uploads" # Pour éviter les problèmes avec le système de fichiers éphémère de Heroku
+  if Rails.env.production? || Rails.env.staging?
+    config.storage = :cloudinary
+    config.cloudinary_credentials = {
+      cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
+      api_key:    ENV['CLOUDINARY_API_KEY'],
+      api_secret: ENV['CLOUDINARY_API_SECRET'],
+    }
+    config.asset_host = "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}"
+  else
+    config.storage = :file
+  end
 end
-
-
-
-# CarrierWave.configure do |config|
-#   config.cache_storage = :file
-
-#   if Rails.env.production? || Rails.env.staging?
-#     config.storage = :cloudinary
-#     config.cloudinary_credentials = {
-#       cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-#       api_key:    ENV['CLOUDINARY_API_KEY'],
-#       api_secret: ENV['CLOUDINARY_API_SECRET'],
-#     }
-#     config.asset_host = "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}"
-#   else
-#     config.storage = :file
-#   end
-# end
