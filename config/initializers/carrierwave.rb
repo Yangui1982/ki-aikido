b0892146db783329484f787e56fcc3692f7f51bf
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2024-09-06 19:43:08 +0200

    config carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,32 @@
-CarrierWave.configure do |config|
-  config.cache_storage = :file
+require 'carrierwave/orm/activerecord'
 
-  if Rails.env.production? || Rails.env.staging?
-    config.storage = :cloudinary
-    config.cloudinary_credentials = {
-      cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-      api_key:    ENV['CLOUDINARY_API_KEY'],
-      api_secret: ENV['CLOUDINARY_API_SECRET'],
-    }
-    config.asset_host = "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}"
-  else
-    config.storage = :file
-  end
+CarrierWave.configure do |config|
+  config.storage = :cloudinary
+  config.cloudinary_credentials = {
+    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
+    api_key:    ENV['CLOUDINARY_API_KEY'],
+    api_secret: ENV['CLOUDINARY_API_SECRET'],
+    secure: true
+  }
+
+  # Dossier par défaut pour le stockage des fichiers
+  config.cache_dir = "#{Rails.root}/tmp/uploads" # Pour éviter les problèmes sur Heroku avec le filesystem
+  config.asset_host = ENV['CLOUDINARY_URL'] if ENV['CLOUDINARY_URL']
 end
+
+
+# CarrierWave.configure do |config|
+#   config.cache_storage = :file
+
+#   if Rails.env.production? || Rails.env.staging?
+#     config.storage = :cloudinary
+#     config.cloudinary_credentials = {
+#       cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
+#       api_key:    ENV['CLOUDINARY_API_KEY'],
+#       api_secret: ENV['CLOUDINARY_API_SECRET'],
+#     }
+#     config.asset_host = "https://res.cloudinary.com/#{ENV['CLOUDINARY_CLOUD_NAME']}"
+#   else
+#     config.storage = :file
+#   end
+# end
